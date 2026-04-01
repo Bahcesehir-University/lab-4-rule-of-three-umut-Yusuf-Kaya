c49b9f7cd3af037e0fcd329f1b8cb3a2036eec9c
umut-Yusuf-Kaya <kayau5234@gmail.com>
2026-04-01 16:46:37 +0300

    Implement constructor, destructor, and copy methods

--- MainProgram.cpp
+++ MainProgram.cpp
@@ -63,9 +63,13 @@ public:
     DynamicBuffer(const char* input) {
         // TODO: Implement constructor
         //   1. Increment g_constructorCount
+        g_constructorCount++;
         //   2. Calculate length of input using strlen()
+        m_length=strlen(input);
         //   3. Allocate m_data with new char[m_length + 1]
+        m_data = new char[m_length + 1];
         //   4. Copy input into m_data using strcpy()
+        strcpy(m_data,input);
     }
 
     // --------------------------------------------------
@@ -76,7 +80,10 @@ public:
     ~DynamicBuffer() {
         // TODO: Implement destructor
         //   1. Increment g_destructorCount
+         g_destructorCount++;
         //   2. Delete the dynamically allocated array (delete[])
+        delete[] m_data;
+        
     }
 
     // --------------------------------------------------
@@ -88,27 +95,37 @@ public:
     DynamicBuffer(const DynamicBuffer& other) {
         // TODO: Implement copy constructor
         //   1. Increment g_copyConstructorCount
+        g_copyConstructorCount++;
         //   2. Copy m_length from other
+        m_length = other.m_length;
         //   3. Allocate new memory: new char[m_length + 1]
+         m_data = new char[m_length + 1]; 
         //   4. Copy string data using strcpy()
+         strcpy(m_data, other.m_data);  
     }
 
     // --------------------------------------------------
     // Copy Assignment Operator (Deep Copy)
     // --------------------------------------------------
-    // - Check for self-assignment
-    // - Free existing memory
+    // - Check for self-assignment    // - Free existing memory
     // - Allocate new memory and copy content from 'other'
     // - Increment g_assignmentCount
     // - Return *this
     DynamicBuffer& operator=(const DynamicBuffer& other) {
         // TODO: Implement copy assignment operator
         //   1. Increment g_assignmentCount
+        g_assignmentCount++;
         //   2. Check for self-assignment (if this == &other)
+         if (this == &other) 
+         return *this; 
         //   3. Delete old m_data (delete[])
+        delete[] m_data;
         //   4. Copy m_length from other
+        m_length = other.m_length;
         //   5. Allocate new memory: new char[m_length + 1]
+        m_data = new char[m_length + 1];
         //   6. Copy string data using strcpy()
+        strcpy(m_data, other.m_data);
         //   7. Return *this
         return *this;
     }
@@ -128,9 +145,14 @@ public:
     void setData(const char* newData) {
         // TODO: Implement setData
         //   1. Delete old m_data (delete[])
+        delete[] m_data;
         //   2. Calculate new length with strlen()
+        m_length = strlen(newData);
         //   3. Allocate new memory: new char[m_length + 1]
+        m_data = new char[m_length + 1];
         //   4. Copy newData into m_data using strcpy()
+        strcpy(m_data, newData);
+        
     }
 
     // --------------------------------------------------
